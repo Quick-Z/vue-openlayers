359db6d52fc4fad52f7dcd03f31fe305e2ccc82a
Rabbit <rabbit__d@sina.com>
2020-02-29 15:59:58 +0800

    初始化，并添加第一个地图demo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <root-nav></root-nav>
     </div>
-    <router-view/>
+    <div class="root-view">
+      <router-view/>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import RootNav from './components/RootNav'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'App',
+  components: {
+    RootNav
   }
 }
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/mixins.scss';
+
+  #app {
+    display: flex;
+  }
+
+  .root-view {
+    flex: 1;
+  }
+
+  // pub
+  .h-title {
+    @include headTitle();
+  }
+  
+  .map-x {
+    @include mapXStyle();
+  }
 </style>
---
+++ src/components/RootNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="g-nav">
+    <router-link
+      class="link"
+      to="/"
+      active-class="active"
+      exact
+    >
+      第一个地图 map
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rootNav'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/css/varibles.scss';
+
+  .g-nav {
+    min-width: 200px;
+    height: 100vh;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    margin-right: 20px;
+    border-right: 1px solid #eee;
+  }
+
+  .link {
+    padding: 10px;
+    border-bottom: 1px dashed #ccc;
+    text-decoration: none;
+    color: $darkTextColor;
+  }
+
+  .active {
+    background: $abgColor;
+    color: #fff;
+    border-bottom: none;
+  }
+</style>
