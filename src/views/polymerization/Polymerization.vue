4ba04179e49f227f3aaf689b7e7dd3b9877a7eb5
Rabbit <rabbit__d@sina.com>
2020-06-01 10:39:58 +0800

    添加聚合数据的注释

--- src/views/polymerization/Polymerization.vue
+++ src/views/polymerization/Polymerization.vue
@@ -27,29 +27,34 @@ export default {
   methods: {
     initMap () {
 
-      let count = 20000
+      let count = 20000 // 点的数量
+      let features = [] // 存放点要素的数组
+    
+      // 初始化每个点的坐标位置
       let e = 4500000
-      let features = []
-      let styleCache = {}
-
       for (let i = 0; i < count; ++i) {
         let coordinates = [2 * e * Math.random() - e, 2* e * Math.random() - e]
+        // 根据坐标生成点状几何
         features[i] = new Feature(new Point(coordinates))
       }
 
+      // 把点状要素放到矢量源里
       let source = new sourceVector({
         features
       })
 
       let clusterSource = new Cluster({
+        // 通过 distance 来控制两个点聚合的间距，如果2个点的间距小于 distance 所设置的数时，就会以聚合的方式显示
         distance: parseInt(40, 10),
-        source
+        source // 就是前面用到的矢量数据源
       })
-
+      
+      // 点的样式
+      let styleCache = {}
       let clusters = new layerVector({
-        source: clusterSource,
-        style: feature => {
-          let size = feature.get('features').length
+        source: clusterSource, // 源，指向上面的聚合数据源
+        style: feature => { // feature 是聚合后的数据，每一个 feature 代表距离小于上面设置的点状要素
+          let size = feature.get('features').length // features 是点状要素
           let style = styleCache[size]
           
           if (!style) {
@@ -64,7 +69,7 @@ export default {
                 })
               }),
               text: new Text({
-                text: size.toString(),
+                text: size.toString(), // 以聚合点的数量作为显示的内容
                 fill: new Fill({
                   color: '#fff'
                 })
@@ -78,6 +83,7 @@ export default {
         }
       })
 
+      // 声明底图
       let raster = new Tile({
         source: new OSM()
       })
